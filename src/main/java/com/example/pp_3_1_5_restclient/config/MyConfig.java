82b7daf8296de3558f2450379071abb891305ba1
Work <79774488441@ya.ru>
2022-09-15 19:16:02 +0300

    Initial commit

---
+++ src/main/java/com/example/pp_3_1_5_restclient/config/MyConfig.java
@@ -0,0 +1,2 @@
+package com.example.pp_3_1_5_restclient.config;public class MyConfig {
+}
---
+++ src/test/java/com/example/pp_3_1_5_restclient/Pp315RestClientApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.pp_3_1_5_restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp315RestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
